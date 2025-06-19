79296d31e61365d6d287bd24ce7a904a60bf8eed
Pranshu Chaudhary <wittywit@users.noreply.github.com>
2025-06-19 21:25:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/testserver',
+  assetPrefix: '/testserver',
   eslint: {
     ignoreDuringBuilds: true,
   },
